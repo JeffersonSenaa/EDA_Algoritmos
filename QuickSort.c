7fe50a65703f08580c539e75039a4fcd13cc31d3
Jefferson12144 <jeffersonsena12144@gmail.com>
2023-02-11 19:42:29 -0300

    Abrindo arquivo csv

--- QuickSort.c
+++ QuickSort.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
-int abreArquivo(FILE *fp, char arquivo[13], char f[2]){
-    fp = fopen(arquivo, f);    //Função para abrir o arquivo
+int main(){
+    FILE *fp, *fpNovo;    //Ponteiro dos arquivo
+    clock_t start, end;   //VAriavel que guarda o valor do tempo de execução em milissegundos
+    char col2[100];
+    int col1, col3;
+
+    start =clock();     //Inicio do tempo de execução
+    fp = fopen("Airlines.csv", "r");    //Função para abrir o arquivo
 
     if(fp == NULL){     //Condição para confirmar abertura do arquivo
         printf("Falha ao abrir o arquivo!\n");
@@ -10,14 +17,13 @@ int abreArquivo(FILE *fp, char arquivo[13], char f[2]){
     else{
         printf("Arquivo aberto com sucesso!\n");
     }
-}
 
-int main(){
-    FILE *fp, *fpNovo;    //Ponteiro dos arquivo
-    clock_t start, end;   //VAriavel que guarda o valor do tempo de execução em milissegundos
+    while(fscanf(fp, "%d, %s, %d", &col1, &col2, &col3) == EOF){
+            printf("%d, %s, %d", col1, col2, col3);
+        }
 
-    start =clock();     //Inicio do tempo de execução
-    abreArquivo(fp, "Airlines.csv", "r");      //Chamada a função para abrir o arquivo
-
+    printf("%s, %d", col2, col3);
+  
+    fclose(fp);         //Fecha o Arquivo
     end = clock();      //Fim do tempo de execução
 }
