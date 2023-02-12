6247d325bc8cfe193229c53c6e6ac71c32b4987d
Jefferson12144 <jeffersonsena12144@gmail.com>
2023-02-11 23:16:26 -0300

    Função fscanf() para percorrer o arquivo

--- QuickSort.c
+++ QuickSort.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <time.h>
-#include <string.h>
+
+typedef struct MediaDelay
+{
+    char companhia[10];
+    int delay;
+    int num;
+};MediaDelay;
+
 
 int main(){
     FILE *fp, *fpNovo;    //Ponteiro dos arquivo
     clock_t start, end;   //VAriavel que guarda o valor do tempo de execução em milissegundos
-    char col2[100];
+    char col2[2];
     int col1, col3;
 
     start =clock();     //Inicio do tempo de execução
     fp = fopen("Airlines.csv", "r");    //Função para abrir o arquivo
-
     if(fp == NULL){     //Condição para confirmar abertura do arquivo
         printf("Falha ao abrir o arquivo!\n");
     }
@@ -18,12 +24,16 @@ int main(){
         printf("Arquivo aberto com sucesso!\n");
     }
 
-    while(fscanf(fp, "%d, %s, %d", &col1, &col2, &col3) == EOF){
-            printf("%d, %s, %d", col1, col2, col3);
-        }
 
-    printf("%s, %d", col2, col3);
-  
+    struct MediaDelay dados;
+
+    while(fscanf(fp, "%d, %s, %d\n", &dados.num, dados.companhia, &dados.delay) != EOF){
+     //   printf("%s", dados.delay);
+    }
+    printf("Valor do ID: %d\n", dados.num);
+    printf("Valor da Companhia: %s\n", dados.companhia);
+    printf("Valor do Delay: %d\n", dados.delay);
+
     fclose(fp);         //Fecha o Arquivo
     end = clock();      //Fim do tempo de execução
 }
